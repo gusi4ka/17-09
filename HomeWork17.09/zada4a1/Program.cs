a8ee710832379806230726a10ebf83cf954b1682
gusi4ka <112167073+gusi4ka@users.noreply.github.com>
2022-09-17 14:12:51 +0300

    Add files via upload

---
+++ HomeWork17.09/zada4a1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.WriteLine("Введите размер массива:  ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[size];
+FillArrayRandomNumbers(numbers);
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100,1000);
+    }
+}
+Console.WriteLine("массив: ");
+PrintArray(numbers);
+int count = 0;
+
+for (int a = 0; a < numbers.Length; a++)
+if (numbers[a] % 2 == 0)
+count++;
+void PrintArray(int[] numbers)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write(numbers[i] + " ");
+}
+    Console.Write("]");
+    Console.WriteLine();
+}
+
+Console.WriteLine($"всего {numbers.Length} ,( {count} )данное кличество из них чётные");
